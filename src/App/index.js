852266e4a4b64bd0e73ea3e358fe16e11043e714
Welly Shen <wellyshen@kkstream.com.tw>
2018-06-26 16:51:35 +0800

    Use create-react-app

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import 'normalize.css';
+import './styles/index.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
