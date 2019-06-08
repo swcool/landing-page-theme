33398f2a28c289b3f28bd0a19e99d3c23a382455
Jaclyn Chen <watertranquil@gmail.com>
2019-06-08 11:06:06 +0800

    Add sponsor and CFP links.

--- 2019/dev/src/components/ActionFooter/index.js
+++ 2019/dev/src/components/ActionFooter/index.js
@@ -4,10 +4,10 @@ import './styles.css';
 
 export default ({}) => (
   <div className="action-footer">
-    <a className="button solid-button">
+    <a className="button solid-button" href='http://cfp.iplayground.io/events/iplayground_2019' target='_blank'>
       我要投稿
     </a>
-    <a className="button hollow-button">
+    <a className="button hollow-button" href='https://drive.google.com/file/d/13NuA3TxuVZ3i0Ayz8uKAuTb9hHhYK_9A/view?usp=drivesdk' target='_blank'>
       企業贊助
     </a>
   </div>
