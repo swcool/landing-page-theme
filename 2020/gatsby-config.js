92ee29dd99208e9f12f09275bd6179674a769733
Steven Hsu <stevenhsusy@gmail.com>
2020-03-31 17:03:10 +0800

    [2020] Fix config.

--- 2020/gatsby-config.js
+++ 2020/gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
+    siteUrl: `https://iplayground.io/2020/public/`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
