eed0813f539c90c46214b6f8ff962eb0f7ccb01f
Steven Hsu <stevenhsusy@gmail.com>
2020-03-31 17:09:20 +0800

    [2020] Modify of prefix.

--- 2020/gatsby-config.js
+++ 2020/gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  pathPrefix: `/2020/public/`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    siteUrl: `https://iplayground.io/2020/public/`,
+    // siteUrl: `https://iplayground.io/2020/public/`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
