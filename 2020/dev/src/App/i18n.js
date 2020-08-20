43ae0c67eab38e7b5a0c0445f8bdba092a9430db
ChiehYiHuang <chiehyi1020@gmail.com>
2020-08-21 00:51:32 +0800

    [UPDATE] test Localization

--- 2020/dev/src/App/i18n.js
+++ 2020/dev/src/App/i18n.js
@@ -1,6 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 import en from './i18n/en.json';
 import tw from './i18n/zh-TW.json';
 
@@ -17,14 +19,15 @@ const resources = {
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
     resources,
 
-    lng: 'zh-TW',             //預設語言
-    fallbackLng: 'zh-TW',     //如果當前切換的語言沒有對應的翻譯則使用這個語言，
+    // fallbackLng: 'zh-TW',     //如果當前切換的語言沒有對應的翻譯則使用這個語言，
+    // lng: 'zh-TW',             //預設語言
 
-    // lng: 'en',             //預設語言
-    // fallbackLng: 'en',     //如果當前切換的語言沒有對應的翻譯則使用這個語言，
+    lng: 'en',             //預設語言
+    fallbackLng: 'en',     //如果當前切換的語言沒有對應的翻譯則使用這個語言，
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
