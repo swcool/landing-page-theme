e914e06d5c0e1a249e017f364cd0e5f148f2c80b
ChiehYiHuang <chiehyi1020@gmail.com>
2020-08-21 02:06:35 +0800

    [UPDATE] test

--- 2020/dev/src/App/index.js
+++ 2020/dev/src/App/index.js
@@ -20,6 +20,10 @@ import './i18n'; // 在这里导入 i18n.js
 import { Trans } from 'react-i18next';
 import i18n from "i18next";
 
+<Trans i18n={i18n}>
+  <App />
+</Trans>
+
 export default class App extends PureComponent {
   state = { showModal: false, whichDay: "day_1" , programs: [], sponsors:null};
 
@@ -228,11 +232,14 @@ export default class App extends PureComponent {
     const mailChimpURL =
       "https://iplayground.us17.list-manage.com/subscribe/post?u=61bc80c5d8118e451c9a5ac80&amp;id=b804a3d8e0";
 
-      const getLanguage = () => {
-        return i18n.language ||
-          (typeof window !== 'undefined' && window.localStorage.i18nextLng) ||
-          'zh-TW';
-      };
+      const languageVar = (typeof window !== 'undefined' && window.localStorage.i18nextLng);
+      i18n.language = languageVar ? languageVar : 'zh-TW';
+
+      // const getLanguage = () => {
+      //   return i18n.language ||
+      //     (typeof window !== 'undefined' && window.localStorage.i18nextLng) ||
+      //     'zh-TW';
+      // };
 
     return (
       <div className="app-fullscreen">
