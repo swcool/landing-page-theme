092645e78283f5546a3012d52cd7a66f5576a2d5
Gerry <gerry@innotech.me>
2020-09-25 09:41:36 +0800

    change style

--- 2020/dev/src/components/ModalContentSpeakers/index.js
+++ 2020/dev/src/components/ModalContentSpeakers/index.js
@@ -6,7 +6,7 @@ export default ({ video_url, alt, speakers, position, title, topic, abstract, on
   <div className="modal-content-speakers">
     <a className="modal-content-speakers__close-btn" onClick={onClickCloseBtn} />
     <div className="modal-content-speakers__img-section">
-      <img className="modal-content-speakers__img " src={video_url !== null && video_url !== "" ? video_url : "https://raw.githubusercontent.com/iplayground/SessionData/2020/v1/images/speakers/nopic.png"} alt={alt} />
+      <img className="modal-content-speakers__img" src={video_url !== null && video_url !== "" ? video_url : "https://raw.githubusercontent.com/iplayground/SessionData/2020/v1/images/speakers/nopic.png"} alt={alt} />
     </div>
     <div className="modal-content-speakers__txt-section">
       <p className="modal-content-speakers__name"><strong>{speakers[0].name}</strong></p>
