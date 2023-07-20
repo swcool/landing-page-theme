95f2d1c0de6cbae35ed4ee39475ebf683b2e3b30
Gerry <gerrykiki@gmail.com>
2023-07-20 17:41:18 +0800

    change

--- 2023/dev/src/components/ActionButton/index.js
+++ 2023/dev/src/components/ActionButton/index.js
@@ -1,15 +1,9 @@
-import React, { PureComponent } from "react";
-import "./styles.css";
+import React from 'react';
 
-export default class ActionButton extends PureComponent {
-  render() {
-    return (
-      <div className="action-button">
-        <div className="action-button-container">
-          <div className="action-button-font-container">{this.props.content}</div>
-        </div>
-        <div className="action-button-container-layer"></div>
-      </div>
-    );
-  }
-}
+import './styles.css';
+
+export default ({title,link}) => (
+  <a className="action-button" href={link} target='_blank'>
+      {title}
+  </a>
+);
